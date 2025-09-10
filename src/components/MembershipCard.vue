b6cf4777add9a66a0ebe176cee625541648c90bf
RobotHanzo <36107150+RobotHanzo@users.noreply.github.com>
2025-09-10 23:43:25 +0800

    QR fix attempt

--- src/components/MembershipCard.vue
+++ src/components/MembershipCard.vue
@@ -1,26 +1,23 @@
 <template>
   <q-card class="q-ma-sm bordered card">
-    <q-img src="card_bg.png" alt="Membership background" />
+    <q-img src="card_bg.png" alt="Membership background" ref="card" />
     <transition
       enter-active-class="animated fadeIn slower"
       leave-active-class="animated fadeOut slower"
     >
       <img class="watermark" v-if="watermark" src="card_stsa.png" alt="Membership watermark" />
     </transition>
-    <vue-qrcode
-      v-if="code"
-      :options="{
-        errorCorrectionLevel: 'Q',
-        width: 0,
-        color: {
-          dark: '#000000ff',
-          light: '#ffffff00',
-        },
-      }"
-      :value="props.code"
-      tag="svg"
+    <qrcode-svg
+      value="oeiwjrfunsfkieruoijedw"
+      level="H"
       class="qrcode"
-    ></vue-qrcode>
+      background="#00000000"
+      :gradient="true"
+      gradient-start-color="#305a7b"
+      gradient-end-color="#8b1c21"
+      :size="qrSize"
+      :dummy="qrSize"
+    />
     <div class="absolute-bottom text-white" style="text-align: right; margin: min(4vw, 40px)">
       <div style="font-size: min(5vw, 60px)">{{ props.name }}</div>
       <div style="font-size: min(2.5vw, 30px)">會員</div>
@@ -29,17 +26,22 @@
 </template>
 
 <script setup lang="ts">
-import VueQrcode from '@chenfengyuan/vue-qrcode';
+import { QrcodeSvg } from 'qrcode.vue';
 import { useQuasar } from 'quasar';
-import { ref } from 'vue';
+import type { ComponentPublicInstance } from 'vue';
+import { computed, ref, useTemplateRef, watch } from 'vue';
 
 const $q = useQuasar();
-const mobileScale = Math.min($q.screen.height / $q.screen.width - 0.2, 1.5);
+const mobileScale = computed(() => Math.min($q.screen.height / $q.screen.width - 0.2, 1.5));
 const watermark = ref(true);
+const card = useTemplateRef<ComponentPublicInstance>('card');
+const qrSize = ref(0);
 const props = defineProps<{
   name: string;
   code: string;
 }>();
+watch(card, calcQRSize, { deep: true });
+window.addEventListener('resize', calcQRSize);
 
 function watermarkAnimation() {
   setTimeout(() => {
@@ -49,6 +51,13 @@ function watermarkAnimation() {
       watermarkAnimation();
     }, 1000);
   }, 1250);
+}
+
+function calcQRSize() {
+  if (card.value) {
+    const el = card.value.$el;
+    qrSize.value = Math.max(el.offsetWidth, el.offsetHeight) * 0.35;
+  }
 }
 
 watermarkAnimation();
@@ -71,8 +80,8 @@ watermarkAnimation();
 .qrcode {
   background-color: transparent;
   position: absolute;
-  left: 55.5%;
-  top: 5.9%;
+  left: 60.2%;
+  top: 13.5%;
   scale: 0.8;
 }
 @media (orientation: portrait) {
